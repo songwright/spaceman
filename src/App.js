d059bdf39f13a6903bc4ef9d715381231e80d4b5
Ramon Sanchez <songwright66@gmail.com>
2020-03-11 18:07:53 -0700

    Added satellite object to Model in App.js

--- src/App.js
+++ src/App.js
@@ -21,6 +21,20 @@ export default class spaceman extends React.Component {
       <View>
         <AmbientLight instensity={ 2.5 } />
         <Pano source={asset(this.state.background)}/>
+        <Model
+          source={{
+            obj: asset('/nasa-aqua-satellite-obj/nasa-aqua-satellite.obj'),
+            mtl: asset('/nasa-aqua-satellite-obj/nasa-aqua-satellite.mtl')
+          }}
+          style={{
+            transform: [
+              { translate: [-1, 0, -0.75] },
+              { scale: 0.075 },
+              { rotateX: 180 },
+              { rotateY: -360 },
+            ]
+          }}
+        />
       </View>
     );
   }
